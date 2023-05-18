dd499a8eb8745394e18b80541f9bcd6611c9102a
Aparna1612 <121945974+Aparna1612@users.noreply.github.com>
2023-05-18 17:34:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,100 +1,32 @@
+# Github Workflow Example
+# Based on and with thanks:   https://wahlnetwork.com/2020/05/12/continuous-integration-with-github-actions-and-terraform/
+
+
 terraform {
   required_version = "~> 1.3.1"
   required_providers {
-    "newrelic" = {
+    newrelic = {
       source  = "newrelic/newrelic"
+      #version = "~> 2.22.1"
     }
   }
 }
 provider "newrelic" {
-  account_id = 3826875
-  api_key = "NRAK-BY52LCHM4JNZWY59IXOZXW7J73A"
   region = "US"                   
 }
 
 terraform {
   backend "s3" {
     bucket = "alo-newrelic-tf-state-test"
-    key    = "alo-newrelic-tf-state-test/policy.tfstate"
+    key    = "alo-newrelic-tf-state-test/newrelic.tfstate"
     region = "us-east-1"
     profile = "default"
   }
 }
 
+# --- Actual new relic terraform here, try changing the policy name!
 
-
-
-
-#newrelic_alert_condition
-data "newrelic_entity" "app" {
-  name = "FoodME"
-  type = "APPLICATION"
-  domain = "APM"
-}
-
-resource "newrelic_alert_policy" "alert_policy_git" {
-  name = "alert_policy_git_demo"
-}
-
-resource "newrelic_alert_condition" "alert_policy_git" {
-  policy_id = newrelic_alert_policy.alert_policy_git.id
-
-  name        = "alert_condition_git"
-  type        = "apm_app_metric"
-  entities    = [data.newrelic_entity.app.application_id]
-  metric      = "apdex"
-  #runbook_url = "https://www.example.com"
-  condition_scope = "application"
-
-  term {
-    duration      = 5
-    operator      = "below"
-    priority      = "critical"
-    threshold     = "0.75"
-    time_function = "all"
-  }
-}
-
-
-resource "newrelic_notification_destination" "email" {
-  account_id = 3826875
-  name = "email-example"
-  type = "EMAIL"
-
-  property {
-    key = "email"
-    value = "aparna@litmus7.com"
-  }
-}
-resource "newrelic_notification_channel" "email" {
-  account_id = 3826875
-  name = "email-example"
-  type = "EMAIL"
-  destination_id = newrelic_notification_destination.email.id
-  product = "IINT"
-
-  property {
-    key = "subject"
-    value = "New Subject Title"
-  }
-}
-
-resource "newrelic_workflow" "foo" {
-  name = "workflow-git_demo"
-  muting_rules_handling = "NOTIFY_ALL_ISSUES"
-
-  issues_filter {
-    name = "filter-name"
-    type = "FILTER"
-
-    predicate {
-      attribute = "labels.policyIds"
-      operator = "EXACTLY_MATCHES"
-      values = [ newrelic_alert_policy.alert_policy_git.id ]
-    }
-  }
-
-  destination {
-    channel_id = newrelic_notification_channel.email.id
-  }
+resource "newrelic_alert_policy" "workflowtest" {
+  name = "Example Github Workflow"
+  incident_preference = "PER_POLICY" 
 }
