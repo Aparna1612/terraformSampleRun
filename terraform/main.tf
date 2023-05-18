5a076fcefa8e825d7d8a88fa282daaf193ab70a8
Aparna1612 <121945974+Aparna1612@users.noreply.github.com>
2023-05-18 15:42:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    newrelic = {
+    registry.terraform.io/newrelic/newrelic = {
       source  = "newrelic/newrelic"
       version = "~> 3.23"
      #constraints = "~> 3.23"
