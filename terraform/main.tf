01c29b8601f65bc0680b4bcbdca9c5aef35f10de
Aparna1612 <121945974+Aparna1612@users.noreply.github.com>
2023-05-19 12:52:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,25 +7,21 @@ terraform {
   required_providers {
     newrelic = {
       source  = "newrelic/newrelic"
-      version = "~>  v1.37.0"
+      version = "~> 3.23"
     }
   }
 }
 provider "newrelic" {
-  region = "US"                   
+  account_id = 3826875
+  api_key = "NRAK-BY52LCHM4JNZWY59IXOZXW7J73A"
+  region = "US"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "alo-newrelic-tf-state-test"
-    key    = "alo-newrelic-tf-state-test/newrelic.tfstate"
-    region = "us-east-1"
-    profile = "default"
-  }
-}
 
 # --- Actual new relic terraform here, try changing the policy name!
 resource "newrelic_alert_policy" "workflowtest" {
-  name = "Example Github Workflow"
-  incident_preference = "PER_POLICY" 
+  name = "Example Github Workflow terraform"
+  incident_preference = "PER_POLICY"
+}
+
 }
